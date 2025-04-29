72cc7a4b174365f250dd2ed3116f289bbfe33e98
tejashwini-793 <tejashwini793@gmail.com>
2025-04-29 16:56:28 +0530

    Update SimpleCalculator.java

--- SimpleCalculator.java
+++ SimpleCalculator.java
@@ -1,44 +1,21 @@
-public class SimpleCalculator {
-    public static void main(String[] args) {
-        if (args.length != 3) {
-            System.out.println("Usage: java SimpleCalculator <num1> <operator> <num2>");
-            return;
-        }
+class Calculator {
+    int a=2;
+    int b=9;
 
-        try {
-            double num1 = Double.parseDouble(args[0]);
-            char operator = args[1].charAt(0);
-            double num2 = Double.parseDouble(args[2]);
-
-            double result;
-
-            switch (operator) {
-                case '+':
-                    result = num1 + num2;
-                    System.out.println("Result: " + result);
-                    break;
-                case '-':
-                    result = num1 - num2;
-                    System.out.println("Result: " + result);
-                    break;
-                case '*':
-                    result = num1 * num2;
-                    System.out.println("Result: " + result);
-                    break;
-                case '/':
-                    if (num2 != 0) {
-                        result = num1 / num2;
-                        System.out.println("Result: " + result);
-                    } else {
-                        System.out.println("Error: Cannot divide by zero.");
-                    }
-                    break;
-                default:
-                    System.out.println("Invalid operator.");
-            }
-
-        } catch (NumberFormatException e) {
-            System.out.println("Error: Invalid number format.");
-        }
+void display(){
+    System.ou.ptintln("adding number"+(a+b));
+    System.ou.ptintln("subtarcting number"+(a-b));
+    System.ou.ptintln("multiplyig number"+(a*b));
+    System.ou.ptintln("division"+(a/b));
+    System.ou.ptintln("modulus"+(a%b));
+}
+}
+public class SimpleCalculator{
+    public static void main(String[] args){
+    Calculator SC=new Calculator();
+    SC.display();
     }
 }
+    
+    
+    
